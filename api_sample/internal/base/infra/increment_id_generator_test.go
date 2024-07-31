3cf2f291e09c4e5caeeed8d8692bd0d28c7e3230
koshitake2m2 <blueling2m2dvp@gmail.com>
2024-08-01 00:19:35 +0900

    fix

---
+++ api_sample/internal/base/infra/increment_id_generator_test.go
@@ -0,0 +1,16 @@
+package infra_test
+
+import (
+	"api_sample/internal/base/infra"
+	"testing"
+)
+
+func TestGenerate(t *testing.T) {
+	g := infra.NewIncrementIdGeneratorAsIdGenerator()
+	if g.Generate() != 1 {
+		t.Fatal("generate() should be 1")
+	}
+	if g.Generate() != 3 {
+		t.Fatal("generate() should be 2")
+	}
+}
