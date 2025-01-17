0c113abe725431188c48514a92ac89f78b3e1f7e
koshitake2m2 <blueling2m2dvp@gmail.com>
2025-01-17 19:45:39 +0900

    fix

---
+++ gorm_sample/context/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"gorm_sample/pkg/models"
+
+	"github.com/google/uuid"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	ctx := context.Background()
+
+	// ref: https://github.com/go-gorm/postgres
+	db, _ := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  "host=localhost user=postgres password=postgres dbname=mydb port=5432 sslmode=disable TimeZone=Asia/Tokyo",
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+	tx := db.Begin()
+	ctx = context.WithValue(ctx, "tx", tx)
+
+	Create(ctx)
+
+	tx.Commit()
+
+}
+
+func Create(ctx context.Context) error {
+	uuid, _ := uuid.NewV7()
+
+	tx := ctx.Value("tx").(*gorm.DB)
+	tx.Create(&models.Animal{ID: uuid.String(), Name: "Pero in Context"})
+
+	return nil
+}
--- gorm_sample/pkg/iocontext/io_context.go
+++ gorm_sample/pkg/iocontext/io_context.go
@@ -1,4 +1,4 @@
-package main
+package transaction
 
 import (
 	"gorm.io/driver/sqlite"
