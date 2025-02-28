9661179ac6c76a14ed4b1d014e8cdf95b7a2f3a0
koshitake2m2 <blueling2m2dvp@gmail.com>
2025-02-28 19:42:21 +0900

    time

---
+++ go_sample2/time/time.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	parseDate()
+	addMonth()
+}
+
+func parseDate() {
+	tstr, _ := time.Parse("2006-01", "2021-01")
+	fmt.Println(tstr) // 2021-01-01 00:00:00 +0000 UTC
+
+	tstr2, _ := time.Parse("2006-01-02 15:04:05", "2021-01-02 15:04:05")
+	fmt.Println(tstr2) // 2021-01-02 15:04:05 +0000 UTC
+}
+
+func addMonth() {
+	tstr2, _ := time.Parse("2006-01-02", "2021-12-02")
+	fmt.Println(tstr2) // 2021-12-02 00:00:00 +0000 UTC
+
+	tstr3 := time.Date(tstr2.Year(), tstr2.Month()+1, tstr2.Day(), 0, 0, 0, 0, time.UTC)
+	fmt.Println(tstr3) // 2022-01-02 00:00:00 +0000 UTC
+
+	tstr4 := tstr2.AddDate(0, 2, 0)
+	fmt.Println(tstr4) // 2022-02-02 00:00:00 +0000 UTC
+}
